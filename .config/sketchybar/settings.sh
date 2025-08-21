b29d00983d6f8373d08cfab299715bafee246dd7
DaffaAhmadSM <Daffamubaroqi321@gmail.com>
2025-08-21 17:39:56 +0700

    add sketchybar config

---
+++ .config/sketchybar/settings.sh
@@ -0,0 +1,23 @@
+FONT="GeistMono Nerd Font"
+
+bar=(
+  position=top
+  height=40
+  background.color=0x00000000
+  background.padding_left=0
+  background.padding_right=0
+  background.corner_radius=0
+)
+
+default=(
+  padding_left=5
+  padding_right=5
+  icon.font="$FONT:Bold:14.0"
+  label.font="$FONT:Regular:13.0"
+  icon.color=0xffffffff
+  label.color=0xffffffff
+  icon.padding_left=4
+  icon.padding_right=4
+  label.padding_left=4
+  label.padding_right=4
+)
